2087efd48562fd85d0aee8589d613a3e7e3637ed
jigneshn <jigneshn@infopercept.com>
2026-02-18 00:11:12 +0530

    chore: update dependencies and migrate to new app structure
    - Updated dependencies in package.json to latest versions, including:
      - @react-native/new-app-screen to 0.84.0
      - react to 19.2.3
      - react-native to 0.84.0
      - Updated various React Native community packages to 20.1.0 and 0.84.0
      - Updated types for react-test-renderer to 19.2.3
      - Changed Node engine requirement to >= 22.11.0
    
    - Removed old App.tsx and constants.ts files, replacing them with a new App.tsx file that utilizes the NewAppScreen component.
    - Deleted audio files from assets as they are no longer needed.

--- SpotifyMusic/index.js
+++ SpotifyMusic/index.js
@@ -1,13 +1,9 @@
 /**
  * @format
-*/
+ */
 
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import TrackPlayer from 'react-native-track-player';
-import { playbackService } from './musicPlayerServices';
 
-// Register the playback service
-TrackPlayer.registerPlaybackService(() => playbackService);
 AppRegistry.registerComponent(appName, () => App);
